acfecf6ee678214d9f4e1e7429e14fd2912d27ab
Виктория <v.chernakova@sibstrin.ru>
2026-06-03 22:01:56 +0700

    Repositories

---
+++ BookStore/Domain/Repositories/IBookRepository.cs
@@ -0,0 +1,16 @@
+﻿using Domain.Entities;
+
+namespace Domain.Repositories;
+
+public interface IBookRepository
+{
+    Task<Book?> GetByIdAsync(Guid id);
+
+    Task<IEnumerable<Book>> GetAllAsync();
+
+    Task AddAsync(Book book);
+
+    Task UpdateAsync(Book book);
+
+    Task DeleteAsync(Guid id);
+}
---
+++ BookStore/Domain/Repositories/ICustomerRepository.cs
@@ -0,0 +1,16 @@
+﻿using Domain.Entities;
+
+namespace Domain.Repositories;
+
+public interface ICustomerRepository
+{
+    Task<Customer?> GetByIdAsync(Guid id);
+
+    Task<IEnumerable<Customer>> GetAllAsync();
+
+    Task AddAsync(Customer customer);
+
+    Task UpdateAsync(Customer customer);
+
+    Task DeleteAsync(Guid id);
+}
